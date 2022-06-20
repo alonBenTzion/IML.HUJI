ba055958682326c1658d0d88bc99d1a53717001c
Alon Ben Zion <alonbentzi@gmail.com>
2022-06-20 16:21:17 +0300

    improving fit

--- IMLearn/desent_methods/gradient_descent.py
+++ IMLearn/desent_methods/gradient_descent.py
@@ -45,7 +45,7 @@ class GradientDescent:
                  tol: float = 1e-5,
                  max_iter: int = 1000,
                  out_type: str = "last",
-                 callback: Callable[[GradientDescent, ...], None] = default_callback):
+                 callback: Callable[[GradientDescent, ], None] = default_callback):
         """
         Instantiate a new instance of the GradientDescent class
 
